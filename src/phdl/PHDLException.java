ed51e7427edb9225994e23bcc741894c88ac3757
byupcbhdl <byupcbhdl@b37ce9da-ff71-45b3-8156-ea48f2632f13>
2011-06-09 22:10:50 +0000

    Created some exceptions and a new package phdl.parser for the intermediary data structures.  Created basic structure of Declarations.

---
+++ src/phdl/PHDLException.java
@@ -0,0 +1,8 @@
+package phdl;
+
+public abstract class PHDLException extends Exception {
+	
+	public abstract String getMessage();
+	public abstract String getExceptionType();
+	
+}
---
+++ src/phdl/parser/DeviceDecl.java
@@ -0,0 +1,7 @@
+package phdl.parser;
+
+public class DeviceDecl extends Declaration {
+	
+	
+	
+}
