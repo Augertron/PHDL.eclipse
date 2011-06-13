e805039d570f6f74a7d84fc382c1204130e739c0
briching <briching@b37ce9da-ff71-45b3-8156-ea48f2632f13>
2011-06-13 17:03:32 +0000

    Added exception classes.

---
+++ trunk/src/phdl/exception/AssignmentWidthException.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2011 BYU Configurable Computing Lab
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package phdl.exception;
+
+import phdl.parser.Element;
+
+public class AssignmentWidthException extends SyntaxException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4902261808672606812L;
+
+	public AssignmentWidthException(Element e) {
+		super(e.getLocation() + " Invalid assignment width detected: "
+				+ e.getName());
+	}
+}
