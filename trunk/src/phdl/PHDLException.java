b67bfc547639758b3f2f34d0cbdf29b5e2c2bea4
briching <briching@b37ce9da-ff71-45b3-8156-ea48f2632f13>
2011-06-13 17:10:23 +0000

    Added necessary override annotations to suppress warnings

--- trunk/src/phdl/PHDLException.java
+++ trunk/src/phdl/PHDLException.java
@@ -2,6 +2,11 @@ package phdl;
 
 public abstract class PHDLException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7816636173565490476L;
+
 	@Override
 	public abstract String getMessage();
 
--- trunk/src/phdl/UndrivenPinException.java
+++ trunk/src/phdl/UndrivenPinException.java
@@ -2,41 +2,46 @@ package phdl;
 
 public class UndrivenPinException extends PHDLException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8096832829702218019L;
 	int lineNum;
 	String instName;
-	
+
 	public UndrivenPinException() {
 		super();
 		lineNum = -1;
 		instName = null;
 	}
-	
+
 	public UndrivenPinException(int lineNum) {
 		super();
 		this.lineNum = lineNum;
 		this.instName = null;
 	}
-	
+
 	public UndrivenPinException(int lineNum, String instName) {
 		super();
 		this.lineNum = lineNum;
 		this.instName = instName;
 	}
-	
+
+	@Override
 	public String getMessage() {
 		String myString = "";
 		if (lineNum == -1) {
 			myString = "A pin is not being driven";
-		}
-		else if (instName == null) {
+		} else if (instName == null) {
 			myString = "Pin on line " + lineNum + " is not being driven";
-		}
-		else {
-			myString = instName + "'s pin on line " + lineNum + " is not being driven";
+		} else {
+			myString = instName + "'s pin on line " + lineNum
+					+ " is not being driven";
 		}
 		return myString;
 	}
-	
+
+	@Override
 	public String getExceptionType() {
 		return "UndrivenPinException";
 	}
