70e2a2275a4d6d54b773bf22c78352d7f58ccabd
briching <briching@b37ce9da-ff71-45b3-8156-ea48f2632f13>
2012-07-28 10:16:26 +0000

    fixed bug in standalone compiler that manifested itself with the upgrade to Xtext 2.3.  Luckily, the developers have provided a copy-and-paste solution until their next maintenance release.  Link: https://bugs.eclipse.org/bugs/show_bug.cgi?id=382555
    Also, I have attempted to start on an eclipse product deployment.  Tutorial I found here: http://kthoms.wordpress.com/tag/eclipse-product/.  Although, Step 4 is the most difficult because there is no way to know for sure if all the dependencies are met until I run the application.  This basically reduces to trial and error.  Hopefully they will make this part of deployment easier in the future.

--- trunk/plugin-2.x/xtext/src/edu/byu/ee/phdl/PhdlRuntimeModule.java
+++ trunk/plugin-2.x/xtext/src/edu/byu/ee/phdl/PhdlRuntimeModule.java
@@ -3,11 +3,58 @@
  */
 package edu.byu.ee.phdl;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.xtext.resource.containers.FlatResourceSetBasedAllContainersState;
+import org.eclipse.xtext.resource.containers.IAllContainersState;
+import org.eclipse.xtext.resource.containers.ResourceSetBasedAllContainersStateProvider;
+
+import com.google.common.collect.Lists;
+
 /**
- * Use this class to register components to be used at runtime / without the Equinox extension
- * registry.
+ * Use this class to register components to be used at runtime / without the
+ * Equinox extension registry.
  */
 public class PhdlRuntimeModule extends edu.byu.ee.phdl.AbstractPhdlRuntimeModule {
+
+	public static class FixedFlatResourceSetBasedAllContainersState extends FlatResourceSetBasedAllContainersState {
+
+		public FixedFlatResourceSetBasedAllContainersState(ResourceSet rs) {
+			super(rs);
+		}
+
+		@Override
+		public Collection<URI> getContainedURIs(String containerHandle) {
+			if (!getHandle().equals(containerHandle))
+				return Collections.emptySet();
+			ResourceSet resourceSet = getResourceSet();
+			List<URI> uris = Lists.newArrayListWithCapacity(resourceSet.getResources().size());
+			URIConverter uriConverter = resourceSet.getURIConverter();
+			for (Resource r : resourceSet.getResources())
+				uris.add(uriConverter.normalize(r.getURI()));
+			return uris;
+		}
+	}
+
+	public static class FixedResourceSetBasedAllContainersStateProvider extends
+			ResourceSetBasedAllContainersStateProvider {
+
+		@Override
+		protected IAllContainersState handleAdapterNotFound(ResourceSet resourceSet) {
+			return new FixedFlatResourceSetBasedAllContainersState(resourceSet);
+		}
+	}
+
+	@Override
+	public Class<? extends IAllContainersState.Provider> bindIAllContainersState$Provider() {
+		return FixedResourceSetBasedAllContainersStateProvider.class;
+	}
 
 	@Override
 	public Class<? extends org.eclipse.xtext.generator.IGenerator> bindIGenerator() {
