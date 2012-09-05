959496d37d1d5d94a802f061ac63a7c2af9e93a4
briching <briching@b37ce9da-ff71-45b3-8156-ea48f2632f13>
2012-09-05 00:20:47 +0000

    refactored phdlnetlist generator.  new builds for standalone compiler (phdlcomp.jar) and translator (phdltran.jar)

--- phdl-2.1/xtext/src/edu/byu/ee/phdl/netlist/PhdlNetlist.java
+++ phdl-2.1/xtext/src/edu/byu/ee/phdl/netlist/PhdlNetlist.java
@@ -3,11 +3,6 @@ package edu.byu.ee.phdl.netlist;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import edu.byu.ee.phdl.elaboration.EConnection;
-import edu.byu.ee.phdl.elaboration.EDesign;
-import edu.byu.ee.phdl.elaboration.EInstance;
-import edu.byu.ee.phdl.elaboration.EPin;
-
 public class PhdlNetlist {
 
 	private final String name;
@@ -15,11 +10,10 @@ public class PhdlNetlist {
 	private final SortedSet<PhdlPart> parts;
 	private final SortedSet<PhdlNet> nets;
 
-	public PhdlNetlist(EDesign design) {
-		this.name = design.getName();
+	public PhdlNetlist(String name) {
+		this.name = name;
 		this.parts = new TreeSet<PhdlPart>();
 		this.nets = new TreeSet<PhdlNet>();
-		buildNetlist(design);
 	}
 
 	public boolean addNet(PhdlNet net) {
@@ -28,24 +22,6 @@ public class PhdlNetlist {
 
 	public boolean addPart(PhdlPart part) {
 		return parts.add(part);
-	}
-
-	private void buildNetlist(EDesign design) {
-		for (String name : design.getRefMap().keySet()) {
-			PhdlPart part = new PhdlPart(name);
-			part.setFootprint(design.getRefMap().get(name).getFootprint());
-			part.setLibrary(design.getRefMap().get(name).getLibrary());
-			addPart(part);
-		}
-		for (EConnection c : design.getNetlist()) {
-			PhdlNet net = new PhdlNet();
-			net.setName(c.getName());
-			for (EPin p : c.getPins()) {
-				PhdlPin pin = new PhdlPin(((EInstance) p.getParent()).getRefDes(), p.getPinMapping());
-				net.addPin(pin);
-			}
-			addNet(net);
-		}
 	}
 
 	public String getName() {
