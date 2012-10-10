8326fa258e5888b03c2b1fea8b581724e497bef9
joshuagm <joshuagm@b37ce9da-ff71-45b3-8156-ea48f2632f13>
2012-10-10 23:51:40 +0000

    Organized Misc Projects folder into folders specifying aproximate version of code.
    Created Folder of working 2.1 examples.

---
+++ misc/projects/Working-2.1-Examples/ptzlanc/pinheader.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<title>BYU PHDL Project - pinheader.phdl</title>
+<?php include("../../../links.php")?>
+<?php include("../../../prettify.php")?>
+
+</head>
+<body onload="prettyPrint()">
+
+	<pre class="prettyprint lang-phdl linenums" title="pinheader.phdl">
+<?php include("pinheader.phdl")?>
+	</pre>
+
+</body>
+</html>
---
+++ misc/projects/Working-2.1-Examples/voicemod/vm_design.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<title>BYU PHDL Project - vm_design.phdl</title>
+<?php include("../../../links.php")?>
+<?php include("../../../prettify.php")?>
+
+</head>
+<body onload="prettyPrint()">
+
+	<pre class="prettyprint lang-phdl linenums" title="vm_design.phdl">
+<?php include("vm_design.phdl")?>
+	</pre>
+
+</body>
+</html>
